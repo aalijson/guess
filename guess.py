c044fc30b210b624cb348dbfd0069c8671b0a2e8
Alan <aalijson@gmail.com>
2022-06-20 18:25:07 +0600

    guess

--- guess.py
+++ guess.py
@@ -1,11 +1,11 @@
 import random   
 NumberToGuess = random.randint(1,100)   
 name = input('Введите ваше имя: ')
-play1 = input('Введите любую клавишу, чтобы продолжить(нет - чтобы завершить): ')
-guesses_made = 0
-userGuess = -1        
+play1 = input('Введите любую клавишу, чтобы начать игру(нет - чтобы завершить): ')
+userGuess = -1
+guesses_made = 0 
 while play1 != 'нет': 
-    userGuess = int(input("Угадай число от 1 до 100: "))    
+    userGuess = int(input("Угадай число от 1 до 100: "))
     guesses_made += 1  
     if userGuess > NumberToGuess:
         print("Число должно быть меньше!")      
@@ -13,4 +13,5 @@ while play1 != 'нет':
         print("Число должно быть больше!")      
     else:         
         print(f'{name}, Вы угадали за {guesses_made} попыток/тки/тку, это число {NumberToGuess}')
-        play1 = input('Хотите продолжить?(нет - чтобы завершить) ')
+        play1 = input('Хотите продолжить(нет - чтобы завершить)? ')
+        guesses_made = 0 
